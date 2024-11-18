1de233f35816b44eae01af0438322056d91f088e
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-11-17 20:41:13 -0600

    Implement Progress Bar UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import ProgressBar from './components/ProgressBar'
 function App() {
   return (
     <>
-     <ProgressBar />
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-900 gap-5">
+        <h1 className="text-center text-2xl font-semibold text-white">Progress Bar (useState)</h1>
+        <ProgressBar />
+      </div>
     </>
   )
 }
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,7 +1,15 @@
 
 const ProgressBar = () => {
   return (
-    <div>ProgressBar</div>
+    <div className="min-w-96 h-[100px] rounded-lg p-5 bg-gray-800" >
+
+    <h3 className="text-white text-center transition-all">{'50%'}</h3>
+
+    <div className="border h-5 p-1">
+      <div className="bg-gradient-to-r from-[#61DAFB] to-[#0e97bd] h-full tr transition-all duration-500"></div>
+    </div>
+
+  </div>
   )
 }
 
